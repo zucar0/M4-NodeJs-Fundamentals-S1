c67951d5abe201ca00b29eb28e9128b1676146f3
Antonio Enríquez <antonio.enralv@gmail.com>
2021-05-21 12:43:39 -0500

    Exportacion de modulos

--- operaciones.js
+++ operaciones.js
@@ -5,7 +5,24 @@ function sumar(num1, num2){
 //Exportamos un archivo
 // module.exports = sumar; 
 
-//Exportación de varios archivos
-module.exports = {
-    sumar
+
+//Reto 1: Agregar resta, multiplicación y división
+// Resta
+function restar(num1,num2){
+    console.log("Restar");
+    console.log(num1-num2);
 }
+// Multiplicación
+function multiplicar(num1,num2){
+    console.log("Multiplicar");
+    console.log(num1*num2);
+}
+//División
+function dividir(num1,num2){
+    console.log("Dividir")
+    console.log(num1/num2)
+}
+module.exports = {
+    restar, multiplicar, dividir, sumar
+}
+
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
-//Modulos
+//Importación del módulo: En la variable operaciones se asigna el require y el nombre del archivo. 
+//Se puede omitir el .js del archivo porque el require hace referencia a los archivos JavaScript, entonces no es necesario
 var operaciones = require('./operaciones');
 operaciones.sumar(2,6);
+operaciones.restar(2,6);
+operaciones.dividir(2,6);
+operaciones.multiplicar(2,6);
