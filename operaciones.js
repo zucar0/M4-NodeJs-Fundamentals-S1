e57e5af376edf7e56a56d01075889be237617968
Antonio Enríquez <antonio.enralv@gmail.com>
2021-05-22 00:25:06 -0500

    Reto 2

--- operaciones.js
+++ operaciones.js
@@ -4,8 +4,6 @@ function sumar(num1, num2){
 }
 //Exportamos un archivo
 // module.exports = sumar; 
-
-
 //Reto 1: Agregar resta, multiplicación y división
 // Resta
 function restar(num1,num2){
@@ -22,15 +20,22 @@ function dividir(num1,num2){
     console.log("Dividir")
     console.log(num1/num2)
 }
-
 function random(min, max, decimals){
     let presition = Math.pow(10, decimals);
     min = min * presition;
     max = max * presition;
     console.log((Math.random()*(max-min+1)+min)/presition);
 }
-
-module.exports = {
-    restar, multiplicar, dividir, sumar, random
+function enteros(min, max){
+    console.log(Math.floor((Math.random()*(max-min +1)+min)));
 }
-
+function randomConReturn(min,max,decimals){
+    return Math.floor((Math.random()*(max-min*1)+min));
+}
+//Número decimal random entre 0.0 y 0.9
+function mathRandom(min, max){
+    console.log(Math.random());
+}
+module.exports = {
+    restar, multiplicar, dividir, sumar, random, enteros, randomConReturn, mathRandom
+}
--- server.js
+++ server.js
@@ -6,3 +6,20 @@ operaciones.restar(2,6);
 operaciones.dividir(2,6);
 operaciones.multiplicar(2,6);
 operaciones.random(1,10,2);
+console.log("Enteros")
+operaciones.enteros(1,10);
+console.log("Random return")
+console.log(operaciones.randomConReturn(1,10,2));
+console.log("Math Random")
+console.log(operaciones.mathRandom());
+
+//Reto 2
+let lista=[];
+aux = 0;
+for(let i=1;i<=100; i++){
+    
+    console.log(aux+1);
+    lista.push(operaciones.random(1,100,2));
+    aux++;
+}
+console.log(lista.length);
